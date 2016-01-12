cfa2e719a980c7d9f76654715d2697910b183f89
Amanda <amandaabaker21@gmail.com>
2016-01-11 19:37:31 -0500

    All functionality added

--- listingController.js
+++ listingController.js
@@ -2,13 +2,28 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+  
 
     /* 
       Implement these functions in the controller to make your application function 
       as described in the assignment spec. 
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      var newListing = {
+        "code": this.code,
+        "name": this.name
+      }
+      $scope.listings.push(newListing);
+        // The idea below was to sort the items after adding the new one
+        // so that they would end up in the correct order, but it does
+        // some wonkey stuff. 
+      //$scope.listings.sort();
+    };
+    $scope.deleteListing = function(index) {
+      $scope.listings.splice(index,1);
+    };
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = $scope.listings[index];
+    };
   }
 ]);
