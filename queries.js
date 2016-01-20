4a43da75c5a89c76361e509a2b12ad7005c830b7
evarku7 <alexp805@ufl.edu>
2016-01-20 13:12:23 -0500

    address for query made

--- queries.js
+++ queries.js
@@ -1,53 +1,37 @@
-/* Fill out these functions using Mongoose queries*/
 
 var fs = require('fs'),
     mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     Listing = require('./ListingSchema.js'),
     config = require('./config.js');
+
 mongoose.connect(config.db.uri);
 
-
 var findLibraryWest = function() {
-  /*
-    Find the document that contains data corresponding to Library West,
-    then log it to the console.
-   */
-
-   Listing.find({ name: "Library West" }, function(err, libWestdata) {
-    if (err) throw err;
-    console.log('\nLibrary West Information:\n' + libWestdata);
+    Listing.find( {name: "Library West"}, function(err, libWestdata) {
+      if (err) throw err;
+      console.log('\n\n\nLibrary West Information:\n\n' + libWestdata);
    });
-
-   //console.log('\nLibrary West Information:\n' + libWestdata);
-
-
 };
+
 var removeCable = function() {
-  /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
-    on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console.
-   */
-   //var cable = Listing.find(  );
-
-   Listing.findOneAndRemove( { code: "CABL" }, function(err) { if (err) throw err; } );
-
-   console.log('\nDeleted the listing!');
-};
-var updatePhelpsMemorial = function() {
-  Listing.findOneAndUpdate({name: "Phelps Laboratory"}, {address: "TESTESTSETSETSETSETSTTSETSETSTSETSETSETSETSETSETSTESTETSE"}, { 'new': true }, function(err, phelps) {
-    if (err) throw err;
-    console.log("\nUpdated Phelps:\n" + phelps);
+    Listing.findOneAndRemove( {code: "CABL"}, function(err) { 
+      if (err) throw err;
+      console.log('\n\n\nDeleted the listing!');
     });
 };
+
+var updatePhelpsMemorial = function() {
+    Listing.findOneAndUpdate({name: "Phelps Laboratory"}, {address: "Phelps Lab, Gainesville, FL 32603, United States"}, {'new': true}, function(err, phelps) {
+      if (err) throw err;
+      console.log("\n\n\nUpdated Phelps:\n\n" + phelps);
+    });
+};
+
 var retrieveAllListings = function() {
-  /*
-    Retrieve all listings in the database, and log them to the console.
-   */
- Listing.find({}, function(err, allData) {
-    if (err) throw err;
-    console.log('\nDatabase Information:\n' + allData);
+    Listing.find({}, function(err, allData) {
+      if (err) throw err;
+      console.log('\n\n\nDatabase Information:\n\n' + allData);
    });
 };
 
