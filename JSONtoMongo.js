09d98b63306a5252ae1dd9812cf8f58d1a950187
Amanda <amandaabaker21@gmail.com>
2016-01-19 23:02:17 -0500

    Properly (hopefully) make script to add buildings

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,41 +1,82 @@
 'use strict';
 /*
-  Import modules/files you may need to correctly run the script.
-  Make sure to save your DB's uri in the config file, then import it with a require statement!
- */
+   Import modules/files you may need to correctly run the script.
+   Make sure to save your DB's uri in the config file, then import it with a require statement!
+   */
 var fs = require('fs'),
     mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     Listing = require('./ListingSchema.js'),
     config = require('./config.js');
+listingData = require('./listings.json');
 
 /* Connect to your database */
 mongoose.connect('mongodb://janedoe:janedoe@ds047315.mongolab.com:47315/software-engineering2');
 
 /*
-  Instantiate a mongoose model for each listing object in the JSON file,
-  and then save it to your Mongo database
- */
-var listingData = fs.readFile( 'listings.json', (err, data) );
-var entires = listingData.entries;
+   Instantiate a mongoose model for each listing object in the JSON file,
+   and then save it to your Mongo database
+   */
+
+var err, data, listingData, entries;
+
+/*fs.readFile( 'listings.json', 'utf-8', function (err, data) {
+  if (err) { return console.error(err); }
+  listingData = JSON.parse(data);
+  });
+//var entires = listingData.entries;
+*/
+console.log(listingData);
+entries = listingData.entries;
+console.log(entries.length);
+
 
 function fillData () {
-    for (i = 0; i < listingData.entries.length; i++){
-        var newEntry = new Listing ({
-            code: entries[i].code,
-            name: entries[i].name,
-            coordinates: {
-                latitude: entries[i].coordinates.latitude,
-                longitude: entries[i].coordinates.longitude
-            },
-            address: entries[i].address
-        });
-
-        newEntry.save();
+  console.log('entered fillData()');
+  for (var i = 0; i < entries.length; i++){
+    if (entries[i].coordinates && entries[i].address) {
+      var newEntry = new Listing ({
+        code: entries[i].code,
+        name: entries[i].name,
+        coordinates: {
+          latitude: entries[i].coordinates.latitude,
+          longitude: entries[i].coordinates.longitude
+        },
+        address: entries[i].address
+      });
     }
-}
+    else if (entries[i].coordinates) {
+      var newEntry = new Listing({
+        code: entries[i].code,
+        name: entries[i].name,
+        coordinates: {
+          latitude: entries[i].coordinates.latitude,
+          longitude: entries[i].coordinates.longitude
+        }
+      });
+    }
+    else if (entries[i].address) {
+      var newEntry = new Listing ({
+        code: entries[i].code,
+        name: entries[i].name,
+        address: entries[i].address
+      });
+    }
+    else {
+      var newEntry = new Listing ({
+        code: entries[i].code,
+        name: entries[i].name
+      })
+    }
+    console.log('almost saved');
+    newEntry.save();
+    console.log('saved');
+  }
+};
+
+fillData();
 
 /*
-  Once you've written + run the script, check out your MongoLab database to ensure that
-  it saved everything correctly.
- */
+   Once you've written + run the script, check out your MongoLab database to ensure that
+   it saved everything correctly.
+   */
